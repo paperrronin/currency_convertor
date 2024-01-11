8c0664877afa7ec70de99c919c53eccd0e9fc431
paperrronin <sateted@proton.me>
2024-01-11 21:35:26 +0200

    add logo, font, fix header styles

--- src/app/core/models/tile.interface.ts
+++ src/app/core/models/tile.interface.ts
@@ -1,6 +1,32 @@
 export interface Tile {
-    color?: string;
     cols: number;
     rows: number;
-    text?: string;
+    data?: string;
+    image?:string;
+    color?: string;
   }
+
+  export const mobileTile:Tile[] = [
+    {cols: 1, rows: 1},
+    {data: 'CURRENCY CONVERTOR', cols: 9, rows: 1},
+    {cols: 2, rows: 1},
+    {data: 'Three', cols: 3, rows: 2, color: 'lightpink'},
+    {cols: 2, rows: 1},
+    {data: 'Five', cols: 3, rows: 2, color: '#FF4040'},
+    {cols: 2, rows: 1},
+    {cols: 2, rows: 1},
+  ]
+    
+
+  export const largeTile:Tile[] = 
+    [
+      {cols: 1, rows: 2},
+      {data: 'CURRENCY CONVERTOR', cols: 3, rows: 2},
+      {cols: 1, rows: 1},
+      {data: 'Three', cols: 2, rows: 2, color: 'lightpink'},
+      {cols: 1, rows: 1},
+      {data: 'Five', cols: 2, rows: 2, color: '#FF4040'},
+      {cols: 1, rows: 1},
+      {cols: 1, rows: 1},
+    ]
+  
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Tile } from 'src/app/core/models/tile.interface';
+import { largeTile, mobileTile, Tile } from 'src/app/core/models/tile.interface';
 import { BreakpointService } from '../services/breakpoint.service';
 
 
@@ -11,6 +11,7 @@ import { BreakpointService } from '../services/breakpoint.service';
 
 
 export class HeaderComponent implements OnInit{
+  tiles:Tile[] = mobileTile
 
   constructor(private breakpoint:BreakpointService) {}
 
@@ -19,37 +20,11 @@ export class HeaderComponent implements OnInit{
     this.renderGrid()
   }
 
-  tiles: Tile[] = [
-    {text: 'One', cols: 4, rows: 2, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 1},
-    {text: 'Three', cols: 2, rows: 2, color: 'lightpink'},
-    {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
-    {text: 'Five', cols: 2, rows: 2, color: '#FF4040'},
-    {text: 'Six', cols: 1, rows: 1, color: '#FFBF40'},
-    {text: 'Seven', cols: 1, rows: 1, color: '#392E85'},
-  ];
-
- 
-  
   renderGrid() {
     if(this.breakpoint.getCurrentBreakpoint() == this.breakpoint.Breakpoints.XSmall){
-      this.tiles[0].cols = 10
-      this.tiles[1].cols = 2
-      this.tiles[2].cols = 3
-      this.tiles[3].cols = 2
-      this.tiles[4].cols = 3
-      this.tiles[5].cols = 2
-      this.tiles[6].cols = 2
+      this.tiles = mobileTile
     } else { 
-      this.tiles = [
-        {text: 'One', cols: 4, rows: 2, color: 'lightblue'},
-        {text: 'Two', cols: 1, rows: 1},
-        {text: 'Three', cols: 2, rows: 2, color: 'lightpink'},
-        {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
-        {text: 'Five', cols: 2, rows: 2, color: '#FF4040'},
-        {text: 'Six', cols: 1, rows: 1, color: '#FFBF40'},
-        {text: 'Seven', cols: 1, rows: 1, color: '#392E85'},
-      ];
+      this.tiles = largeTile
     }
   }
 }

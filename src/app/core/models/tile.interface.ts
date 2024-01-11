d99825fe241456afd4192efb879285b04fd94693
paperrronin <sateted@proton.me>
2024-01-11 22:11:02 +0200

    style fixes

--- src/app/core/models/tile.interface.ts
+++ src/app/core/models/tile.interface.ts
@@ -7,25 +7,25 @@ export interface Tile {
   }
 
   export const mobileTile:Tile[] = [
-    {cols: 1, rows: 1},
-    {data: 'CURRENCY CONVERTOR', cols: 9, rows: 1},
-    {cols: 2, rows: 1},
-    {data: 'Three', cols: 3, rows: 2, color: 'lightpink'},
-    {cols: 2, rows: 1},
-    {data: 'Five', cols: 3, rows: 2, color: '#FF4040'},
-    {cols: 2, rows: 1},
-    {cols: 2, rows: 1},
+    {cols: 3, rows: 1},
+    {data: 'CURRENCY CONVERTOR', cols: 17, rows: 1},
+    {cols: 3, rows: 1},
+    {data: 'Three', cols: 7, rows: 2, color: 'lightpink'},
+    {cols: 3, rows: 1},
+    {data: 'Five', cols: 7, rows: 2, color: '#FF4040'},
+    {cols: 3, rows: 1},
+    {cols: 3, rows: 1},
   ]
     
 
   export const largeTile:Tile[] = 
     [
-      {cols: 1, rows: 2},
-      {data: 'CURRENCY CONVERTOR', cols: 3, rows: 2},
+      {cols: 2, rows: 2},
+      {data: 'CURRENCY CONVERTOR', cols: 8, rows: 2},
       {cols: 1, rows: 1},
-      {data: 'Three', cols: 2, rows: 2, color: 'lightpink'},
+      {data: 'Three', cols: 4, rows: 2, color: 'lightpink'},
       {cols: 1, rows: 1},
-      {data: 'Five', cols: 2, rows: 2, color: '#FF4040'},
+      {data: 'Five', cols: 4, rows: 2, color: '#FF4040'},
       {cols: 1, rows: 1},
       {cols: 1, rows: 1},
     ]

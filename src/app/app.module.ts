2e8a0d39aa55e2f39a2ebda053fb6c618378b206
paperrronin <sateted@proton.me>
2024-01-12 18:23:22 +0200

    feat input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { HeaderComponent } from './shared/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
 import { HttpClientModule } from '@angular/common/http';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from "@angular/core";
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatButtonModule} from '@angular/material/button'
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
 
 
 
 const materialModules = [
     MatGridListModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule, 
+    MatSelectModule
+    
 ];
 
 @NgModule({
